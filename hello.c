bdda048fda2991a6d6fe2ba26c141e5ab526465f
bsmith74 <54595473+bsmith74@users.noreply.github.com>
2019-08-28 13:56:50 -0500

    A simple hello world program

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Brady Smith
+ * Date: 2019/8/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Brady Smith\n");
+
+  return 0;
+}
